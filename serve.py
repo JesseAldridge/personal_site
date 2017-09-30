af32d4d49a9098acce00bc4d0e69e6f87a3ebe44
Jesse Aldridge <JesseAldridge@gmail.com>
2017-09-30 15:32:38 -0700

    add server

---
+++ serve.py
@@ -0,0 +1,13 @@
+import sys
+
+import flask
+
+app = flask.Flask(__name__)
+port = int(sys.argv[1]) if len(sys.argv) == 2 else 80
+
+@app.route('/')
+def index():
+  return flask.render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=(port != 80))
