3ac2f5aba2051d4c35f582451c76259442a52e55
Jesse Aldridge <jessealdridge@gmail.com>
2019-01-01 19:00:40 -0800

    ignore messages

--- serve.py
+++ serve.py
@@ -1,6 +1,7 @@
-import sys
+import sys, json
 
 import flask
+from flask import request
 
 app = flask.Flask(__name__)
 port = int(sys.argv[1]) if len(sys.argv) == 2 else 80
@@ -8,6 +9,13 @@ port = int(sys.argv[1]) if len(sys.argv) == 2 else 80
 @app.route('/')
 def index():
   return flask.render_template("index.html")
+
+
+@app.route('/contact', methods=['POST'])
+def contact():
+  with open('messages.txt', 'a') as f:
+    f.write(json.dumps(request.form) + '\n')
+  return flask.render_template("thank-you.html")
 
 if __name__ == '__main__':
   app.config['TEMPLATES_AUTO_RELOAD'] = True
