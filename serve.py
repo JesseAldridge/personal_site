dd10773c70b7546fd5bf359c477f6709b17b092d
Jesse Aldridge <jessealdridge@gmail.com>
2019-01-01 20:22:25 -0800

    try hardcoding letencrypt url

--- serve.py
+++ serve.py
@@ -20,6 +20,10 @@ def contact():
     f.write(json.dumps(request.form) + '\n')
   return flask.render_template("thank-you.html")
 
+@app.route('.well-known/acme-challenge/SA6HnavVx_VbGUV0LO8lmzavdHpP5Th9plU8DJuE7Dk')
+def well_known():
+  return app.send_static_file('static/.well-known/acme-challenge/SA6HnavVx_VbGUV0LO8lmzavdHpP5Th9plU8DJuE7Dk')
+
 if __name__ == '__main__':
   app.config['TEMPLATES_AUTO_RELOAD'] = True
   app.run(host='0.0.0.0', port=port, debug=(port != 80))
